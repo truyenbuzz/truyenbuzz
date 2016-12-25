37b4dcf4fa7b78fca1601b43d83db13e55d7bcdc
phamtrantri <phamtrantri@gmail.com>
2016-12-25 10:21:26 +0700

    add nav
    add nav

--- src/frontend/js/app/Header.js
+++ src/frontend/js/app/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {Navbar, Nav, NavbarBrand, NavbarHeader, NavbarToggle, NavDropdown, NavItem, MenuItem} from 'react-bootstrap';
 
 class Header extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Navbar inverse collapseOnSelect id="nav">
+        <Navbar.Header id="nav-header">
+          <Navbar.Brand>
+            <a href="/">Truyệnbuzz</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavDropdown eventKey={3} title="Thể loại" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem id="nav-item" eventKey={1} href="/">Đăng nhập</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
