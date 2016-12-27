37b70cc60b2b556fee1265847503bd31de1057b1
phamtrantri <phamtrantri@gmail.com>
2016-12-27 16:31:34 +0700

    test

--- src/frontend/js/stories/Cover.js
+++ src/frontend/js/stories/Cover.js
@@ -9,7 +9,7 @@ class Cover extends React.Component {
           <div className="title"><a id="route-title" href="#"></a>Đế Bá Con Chiên Ngoan Đạo</div>
           <div className="truncate author"><i className="glyphicon glyphicon-pencil"></i>Phạm Trần Trí</div>
           <div className="genre"><a id="route-genre" href="#">TIÊN HIỆP</a></div>
-          <div className="chapter"><i className="glyphicon glyphicon-list"></i>47</div>
+          <div className="chapter"><i className="glyphicon glyphicon-list"></i>42</div>
           <div className="view"><i className="glyphicon glyphicon-eye-open"></i>10123</div>
         </figure>
       </div>
